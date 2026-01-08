4170d89e8904bafb1fcae776a2232eb9910031e7
James Mark Chan <jamesmarkchan@gmail.com>
2026-01-07 22:33:58 -0800

    initial commit

---
+++ Source/BanannaEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class BanannaEditorTarget : TargetRules
+{
+	public BanannaEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.AddRange( new string[] { "Bananna" } );
+	}
+}
